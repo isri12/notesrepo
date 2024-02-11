e4192c9fbac0e7a0393bae67ae44eeb1a36bb6aa
isri12 <itsehayu12@gmail.com>
2024-02-11 11:46:42 -0500

    leet

--- cplusplus_practice_questions.cpp
+++ cplusplus_practice_questions.cpp
@@ -1,7 +1,136 @@
 
 
-// =========Leet 647 Palindromic Substrings============================================================================================
+// If you are a beginner solve these problems which makes concepts clear for future coding:
+// 	1. Two Sum
+// 	2. Roman to Integer
+// 	3. Palindrome Number
+// 	4. Maximum Subarray
+// 	5. Remove Element
+// 	6. Contains Duplicate
+// 	7. Add Two Numbers
+// 	8. Majority Element
+// 	9. Remove Duplicates from Sorted Array
+// Practice them in a row for better understanding and please Upvote for more questions.
 
+
+
+
+//===========leet 741. cherry-pickup 2 ============================================================================================
+//https://leetcode.com/problems/cherry-pickup-ii/description/?envType=daily-question&envId=2024-02-11
+
+
+//===========leet 741. cherry-pickup 1 ============================================================================================
+//https://leetcode.com/problems/cherry-pickup/description/
+
+
+
+
+//===========leet 9. Palindrome Number ============================================================================================
+//https://leetcode.com/problems/palindrome-number/description/
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        std::string str= std::to_string(x);
+        std::string strcpy=str;
+        std::reverse(strcpy.begin(),strcpy.end());
+        if(x < 0) {
+            return false;
+        }else if (str==strcpy){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+int main()
+{
+  Solution s1;
+  int x=  121;
+  int y= -121;
+  int z= 10;
+  std::cout<<s1.isPalindrome(x)<<'\n';
+  std::cout<<s1.isPalindrome(y)<<'\n';
+  std::cout<<s1.isPalindrome(z)<<'\n';
+//working 
+}
+
+//others
+//Approach 1: Reversing the Entire Number
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0) {
+//             return false;
+//         }
+
+//         long long reversed = 0;
+//         long long temp = x;
+
+//         while (temp != 0) {
+//             int digit = temp % 10;
+//             reversed = reversed * 10 + digit;
+//             temp /= 10;
+//         }
+
+//         return (reversed == x);
+//     }
+// };
+//Approach 2: Reversing Half of the Number
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0 || (x != 0 && x % 10 == 0)) {
+//             return false;
+//         }
+
+//         int reversed = 0;
+//         while (x > reversed) {
+//             reversed = reversed * 10 + x % 10;
+//             x /= 10;
+//         }
+//         return (x == reversed) || (x == reversed / 10);
+//     }
+// };
+
+// =========Leet 647 Palindromic Substrings============================================================================================
+// https://leetcode.com/problems/palindromic-substrings/description/ 
 // Given a string s, return the number of palindromic substrings in it.
 // A string is a palindrome when it reads the same backward as forward.
 // A substring is a contiguous sequence of characters within the string.
@@ -23,36 +152,55 @@
 // s consists of lowercase English letters
 
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// #include<string>
 
-class Solution {
-public:
-    int countSubstrings(std::string s) {
-        for (char i:s){
-            std::cout<<i;
-        }
-    std::cout<<"\n";    
-    return 0;
+// class Solution {
+// public:
+//     int countSubstrings(std::string s) {
+//         int count=2;
+//         // std::cout<<s[0]<<'\n';
+//         // std::cout<<s[1]<<'\n';
+//         // std::cout<<s[2]<<'\n';
+//         //std::cout<<s.length()<<'\n';
+
+//         std::string reverseS=s;
+//         std::reverse(reverseS.begin(),reverseS.end());
+//         // for(char j:reverseS){
+//         //     std::cout<<j;
+//         // }
+//     // std::cout<<"\n";
+//     //     for(int k=s.length();k>0;k--){
+//     //         std::cout<<s.at(k);
+//     //     }
+//     std::cout<<"\n";    
+//         for (char i:s){
+           
+//             //count=i[0]
+//             if (s[0]==i){
+//                  std::cout<<i;
+//                  count++;
+//             }
+//         }
+//     std::cout<<"\n";    
+//     return count;
         
-    }
-};
+//     }
+// };
 
 
-int main()
-{
-  Solution s1;
-  std::string str = "abcsapf";
-  s1.countSubstrings(str);
-}
+// int main()
+// {
+//   Solution s1;
+//   std::string str = "abc";
+//   std::cout<<s1.countSubstrings(str);
+// }
 
 
 
-// =====================================================================================================================
-//Leet code
-//1637. Widest Vertical Area Between Two Points Containing No Points
+// ======Leet code 1637. Widest Vertical Area Between Two Points Containing No Points===============================================================================================================
 //https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/description/?envType=daily-question&envId=2023-12-21
 
 // #include <iostream>
@@ -107,49 +255,44 @@ int main()
 //     return 0;
 // }
 
+// /* Hacker rank - For Loop
+// # Practice
+// ## 1) For Loop
+// ### In this challenge, you will use a for loop to increment a variable through a range.
+// #### Input:
+// - You will be given two positive integers, a and b (a =<b ), separated by a newline.
+// #### Output:
+// - For each integer n in the inclusive interval[a,b] :
+//   - if 1=<n=<9 then print english representation of it in lower case. i.e 1 "one", 2 "two" ...
+//   - If n>9 and even number print "even"
+//   - if n>9 and odd number, print "odd"
+// #### Example
+// Input:
+// 8
+// 11
+// output:
+// eight
+// nine
+// even
+// odd
 
-
-
-
-
-/* Hacker rank - For Loop
-# Practice
-## 1) For Loop
-### In this challenge, you will use a for loop to increment a variable through a range.
-#### Input:
-- You will be given two positive integers, a and b (a =<b ), separated by a newline.
-#### Output:
-- For each integer n in the inclusive interval[a,b] :
-  - if 1=<n=<9 then print english representation of it in lower case. i.e 1 "one", 2 "two" ...
-  - If n>9 and even number print "even"
-  - if n>9 and odd number, print "odd"
-#### Example
-Input:
-8
-11
-output:
-eight
-nine
-even
-odd
-
-INPUT:
-3 15
-OUTPUT
-three
-four
-five
-six
-seven
-eight
-nine
-even    //10
-odd    //11
-even    //12
-odd     //13
-even    //14
-odd     //15
-*/
+// INPUT:
+// 3 15
+// OUTPUT
+// three
+// four
+// five
+// six
+// seven
+// eight
+// nine
+// even    //10
+// odd    //11
+// even    //12
+// odd     //13
+// even    //14
+// odd     //15
+// */
 
 
 // #include <iostream>
@@ -352,11 +495,6 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 // {
 //     // Code to calculate and store average grade
 // }
-// ```
-
-
-
-
 
 //Leet code
 //1637. Widest Vertical Area Between Two Points Containing No Points
