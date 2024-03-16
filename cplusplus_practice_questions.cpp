5e1550870984c16dea6200adde884400045b4244
Israel <ikinfu@terpmail.umd.edu>
2024-03-16 08:10:13 -0400

    Update cplusplus_practice_questions.cpp

--- cplusplus_practice_questions.cpp
+++ cplusplus_practice_questions.cpp
@@ -27,69 +27,62 @@
 
 //===========leet 9. Palindrome Number ============================================================================================
 //https://leetcode.com/problems/palindrome-number/description/
-// Given an integer x, return true if x is a 
-// palindrome
-// , and false otherwise.
-
- 
+// Given an integer x, return true if x is a palindrome , and false otherwise.
+//palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run
 
 // Example 1:
-
 // Input: x = 121
 // Output: true
 // Explanation: 121 reads as 121 from left to right and from right to left.
-// Example 2:
 
+// Example 2:
 // Input: x = -121
 // Output: false
 // Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-// Example 3:
 
+// Example 3:
 // Input: x = 10
 // Output: false
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  
 
 // Constraints:
-
 // -231 <= x <= 231 - 1
- 
-
 // Follow up: Could you solve it without converting the integer to a string?
 
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// #include<string>
 
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        std::string str= std::to_string(x);
-        std::string strcpy=str;
-        std::reverse(strcpy.begin(),strcpy.end());
-        if(x < 0) {
-            return false;
-        }else if (str==strcpy){
-            return true;
-        }else{
-            return false;
-        }
-    }
-};
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         std::string str= std::to_string(x);
+//         std::string strcpy=str;
+//         std::reverse(strcpy.begin(),strcpy.end());
+//         if(x < 0) {
+//             return false;
+//         }else if (str==strcpy){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// };
 
-int main()
-{
-  Solution s1;
-  int x=  121;
-  int y= -121;
-  int z= 10;
-  std::cout<<s1.isPalindrome(x)<<'\n';
-  std::cout<<s1.isPalindrome(y)<<'\n';
-  std::cout<<s1.isPalindrome(z)<<'\n';
-//working 
-}
+// int main()
+// {
+//   Solution s1;
+//   int x=  121;
+//   int y= -121;
+//   int z= 10;
+//   std::cout<<s1.isPalindrome(x)<<'\n';
+//   std::cout<<s1.isPalindrome(y)<<'\n';
+//   std::cout<<s1.isPalindrome(z)<<'\n';
+// //working 
+// }
 
 //others
 //Approach 1: Reversing the Entire Number
