5a1b3d68f768883ed168ce6582541ddcfd1b071c
Israel <ikinfu@terpmail.umd.edu>
2024-03-16 07:56:06 -0400

    Update cplusplus_practice_questions.cpp

--- cplusplus_practice_questions.cpp
+++ cplusplus_practice_questions.cpp
@@ -128,6 +128,25 @@ int main()
 //         return (x == reversed) || (x == reversed / 10);
 //     }
 // };
+//===========leet 525. Contiguous Array ============================================================================================
+// https://leetcode.com/problems/contiguous-array/description/?envType=daily-question&envId=2024-03-16
+
+
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+ 
+Example 1:
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+ 
+Example 2:
+Input: nums = [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ 
+
+
+
 
 // =========Leet 647 Palindromic Substrings============================================================================================
 // https://leetcode.com/problems/palindromic-substrings/description/ 
